029b2702331cc738fe81b6454db0781396ce5c41
osalyousef <os.forwork1@gmail.com>
2024-06-19 19:36:22 +0300

    Initial Commit

---
+++ Qattah/QattahApp.swift
@@ -0,0 +1,17 @@
+//
+//  QattahApp.swift
+//  Qattah
+//
+//  Created by Osamah alyousef on 19/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct QattahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
