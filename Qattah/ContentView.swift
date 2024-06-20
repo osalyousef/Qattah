7c8ebc841431fbb3a7ad4576e797b38fba41724a
osalyousef <os.forwork1@gmail.com>
2024-06-20 20:14:33 +0300

    Qattah

--- Qattah/ContentView.swift
+++ Qattah/ContentView.swift
@@ -1,24 +1,71 @@
-//
-//  ContentView.swift
-//  Qattah
-//
-//  Created by Osamah alyousef on 19/06/2024.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var cost = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tip = 15
+    @FocusState private var amountFocus: Bool
+    let tipSelector = [10, 15, 20, 0]
+    var totalPerPerson: Double {
+        let peopleNumPlusTwo = Double(numberOfPeople + 2)
+        let tipSelector_ = Double(tip)
+        let tipValue = cost/100 * tipSelector_
+        let total = cost + tipValue
+        let costPerPerson = total/peopleNumPlusTwo
+        return costPerPerson
+    }
+     var amountPlusTip: Double {
+         let tipSelector_ = Double(tip)
+         let tipValue = cost/100 * tipSelector_
+         let total = cost + tipValue
+         return total
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section("Amount:") {
+                    TextField("Amount:", value: $cost, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountFocus)
+                    Picker("Number of People:", selection: $numberOfPeople) {
+                        ForEach(2..<10) {
+                            Text("\($0) People")
+                        }
+                    }
+                }
+                
+                
+                Section ("How much tip do you want to leave?"){
+                    Picker ("Tip:" , selection: $tip) {
+                        ForEach (tipSelector , id: \.self) {
+                            Text($0 , format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Original Amount + Tip"){
+                    Text(amountPlusTip , format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                Section("Total per person:") {
+                    Text(totalPerPerson , format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .toolbar() {
+                if amountFocus {
+                    Button("Done !") {
+                        amountFocus = false
+                    }
+                }
+            }
+            
+            .navigationTitle("Qattah")
+            
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
